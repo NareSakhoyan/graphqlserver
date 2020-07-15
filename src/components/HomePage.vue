03f8745e6de75a0e504c8e1f7c1ff326394a8e23
PC-3 <nare.sakhoyan@gmail.com>
2020-07-15 20:34:53 +0400

    bug filter

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -4,12 +4,66 @@
             <tbody>
                 <div class="">
                     <tr class="item-header">
-                        <td>Status</td>
-                        <td>Time</td>
+                        <td>
+                            <b-dropdown
+                                    v-model="filterParams.status"
+                                    multiple
+                                    aria-role="list">
+                                <button class="button is-primary" type="button" slot="trigger">
+                                    <span>Status ({{ filterParams.status.length }})</span>
+                                    <b-icon icon="menu-down"></b-icon>
+                                </button>
+                                <div v-for="(statCode, index) in Object.keys(statusCodes)" :key="`statusCode${index}`">
+                                    <b-dropdown-item  :value="statCode" aria-role="listitem">
+                                        <span>{{statusCodes[statCode]}}</span>
+                                    </b-dropdown-item>
+                                </div>
+                            </b-dropdown>
+                        </td>
+                        <td>
+                            <label>Request Date</label>
+                            <b-field label="From" placeholder="Select a date" name="filterParams.requestDate">
+                                <b-datepicker
+                                        placeholder="Click to select..."
+                                        v-model="requestStartDate"
+                                        :max-date="requestEndDate">
+                                </b-datepicker>
+                            </b-field>
+                            <b-field label="to" placeholder="Select a date" name="filterParams.requestDate">
+                                <b-datepicker
+                                        placeholder="Click to select..."
+                                        v-model="requestEndDate"
+                                        :min-date="requestStartDate"
+                                        :max-date="requestEndDate">
+                                </b-datepicker>
+                            </b-field>
+                        </td>
                         <td>Full Name</td>
-                        <td>Birthday</td>
+                        <td>
+                            <label>Birthday</label>
+                            <b-field label="From" placeholder="Select a date" name="filterParams.requestDate">
+                                <b-datepicker
+                                        placeholder="Click to select..."
+                                        v-model="birthdayStartDate"
+                                        :max-date="birthdayEndDate">
+                                </b-datepicker>
+                            </b-field>
+                            <b-field label="to" placeholder="Select a date" name="filterParams.requestDate">
+                                <b-datepicker
+                                        placeholder="Click to select..."
+                                        v-model="birthdayEndDate"
+                                        :min-date="birthdayStartDate"
+                                        :max-date="birthdayEndDate">
+                                </b-datepicker>
+                            </b-field>
+                        </td>
                         <td>Address</td>
-                        <td>Gender</td>
+                        <td>
+                            <b-field label="Gender">
+                                <b-select v-model="filterParams.gender" placeholder="Choose..." name="responseStatus">
+                                    <option v-for="(gender, index) in Object.keys(genders)" :key="`gender${index}`" :value="gender">{{genders[gender]}}</option>
+                                </b-select>
+                            </b-field></td>
                         <td>Company Name</td>
                         <td>Company Number</td>
                         <td>Company Address</td>
@@ -28,6 +82,7 @@
                     </tr>
                 </div>
             </tbody>
+            <b-button class="button" @click="applyFilters">Update</b-button>
         </div>
     </div>
 </template>
@@ -41,14 +96,91 @@
         components: {
             // Item
         },
+        data() {
+          return {
+              statusCodes: {
+                  "400": "Bad request",
+                  "500": "Internal Server Error",
+                  "700": "Maximum Invoke Depth Reached",
+              },
+              genders: {
+                  "M": "Man",
+                  "W": "Woman"
+              },
+              filterParams: {
+                  requestDate: {
+                      start: '',
+                      end: new Date(),
+                  },
+                  birthdayDate: {
+                      start: '',
+                      end: new Date(),
+                  },
+                  status: [],
+                  gender: '',
+              }
+          }
+        },
+        computed: {
+            filters: {
+                get() {
+                    return this.filtersStr
+                },
+                set(v) {
+                    console.log('Setting value to filters', v);
+                }
+            },
+            requestStartDate: {
+                get() {
+                    return this.filterParams.requestDate.start
+                },
+                set(v) {
+                    this.filterParams.requestDate.start = v
+                }
+            },
+            requestEndDate: {
+                get() {
+                    return this.filterParams.requestDate.end
+                },
+                set(v) {
+                    this.filterParams.requestDate.end = v
+                }
+            },
+            birthdayStartDate: {
+                get() {
+                    return this.filterParams.birthdayDate.start
+                },
+                set(v) {
+                    this.filterParams.birthdayDate.start = v
+                }
+            },
+            birthdayEndDate: {
+                get() {
+                    return this.filterParams.birthdayDate.end
+                },
+                set(v) {
+                    this.filterParams.birthdayDate.end = v
+                }
+            },
+            genderModel: {
+                get() {
+                    return this.filterParams.gender
+                },
+                set(v) {
+                    this.filterParams.gender = v
+                }
+            },
+            filtersStr: () => "{\"status\":{\"$in\":[\"200\"]}}"
+        },
         apollo: {
-            items: gql`query {
-                      items {
+            items: {
+                query: gql`query($filters: String) {
+                      items(filters: $filters) {
                         id
                         status
                         createdAt
                         request {
-                          timeStamp \t
+                          timeStamp
                           company {
                             companyName
                             companyNumber
@@ -81,15 +213,17 @@
                           }
                         }
                       }
-                    }`
+                    }`,
+                variables: {
+                    filters: ''
+                }
+            }
         },
         methods: {
             getStatus(statusCode) {
-                console.log(statusCode);
                 return (statusCode == 200)? 'successful': 'unsuccessful'
             },
             getAddress(addressDetails) {
-                console.log(addressDetails);
                 let tmp = addressDetails.address || addressDetails.streetName
                 return `${tmp?tmp+', ': ''}${addressDetails.cityName}`
             },
@@ -107,15 +241,61 @@
                 if (gender === "M") return 'man'
                 if (gender === "W") return 'woman'
                 return 'other'
+            },
+            applyFilters() {
+                // function removeElementsWhichValueIsEmpty(obj) {
+                //     let keys = Object.keys(obj)
+                //     for (let i in keys) {
+                //         let prop = obj.keys[i]
+                //         if (!prop || !prop.length || !Object.keys(prop).length){
+                //             delete obj[prop]
+                //         }
+                //         else if (typeof prop=== "object") {
+                //             // let isArray = Array.isArray(prop)
+                //             // if (!isArray.length) {
+                //             //     delete obj[prop]
+                //             // }
+                //             removeElementsWhichValueIsEmpty(prop)
+                //         }
+                //     }
+                // }
+                let toFilterObject = {
+                    status: {$in: {}},
+                    request: {
+                        employee: {
+                            employeeBirth: {
+                                gender: '',
+                                date: {
+                                    $gt: '',
+                                    $lt: '',
+                                },
+                            }
+                        }
+                    },
+                    createdAt: {},
+                }
+                toFilterObject.status["$in"] = [...this.filterParams.status]
+                toFilterObject.createdAt = {$gt: this.filterParams.requestDate.start, $lt: this.filterParams.requestDate.end}
+                toFilterObject.request.employee.employeeBirth.gender = this.filterParams.gender
+                toFilterObject.request.employee.employeeBirth.date.$gt = this.filterParams.birthdayDate.start
+                toFilterObject.request.employee.employeeBirth.date.$lt = this.filterParams.birthdayDate.end
+                console.log('toFilterObject: ', toFilterObject);
+                let filtersStr = JSON.stringify(toFilterObject)
+                filtersStr = filtersStr.replace(/["]/g, "\\\"")
+                console.log("filterStr: ", filtersStr, "date: ", toFilterObject.request.employee.employeeBirth.date.$gt.toString());
+                // this.$apollo.queries.items.refetch({filters: filtersStr})
             }
         },
+        beforeMount() {
+            this.$apollo.queries.items.options.variables.filters = this.filtersStr;
+        }
     }
 </script>
 
 <style scoped>
     td {
         border: 1px solid black;
-        padding: 3px;
+        padding: 3px 10px 3px 10px;
     }
     .item-header td{
         background-color: #9fdcdc;
